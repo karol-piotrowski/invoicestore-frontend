d040e536a4df5f0f37139ddad228c43d963763f9
karol-piotrowski <kp0@o2.pl>
2019-06-21 02:15:53 +0200

    Initial commit with project structure

---
+++ src/main/java/com/kodilla/invoicestorefrontend/InvoicestoreFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.kodilla.invoicestorefrontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvoicestoreFrontendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InvoicestoreFrontendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kodilla/invoicestorefrontend/InvoicestoreFrontendApplicationTests.java
@@ -0,0 +1,16 @@
+package com.kodilla.invoicestorefrontend;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InvoicestoreFrontendApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
